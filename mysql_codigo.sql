07c034dc1866c3129bf4d08c3490371ceaed85f5
guest05 <114414067+guest05@users.noreply.github.com>
2022-10-25 11:03:41 -0300

    Add files via upload

---
+++ mysql_codigo.sql
@@ -0,0 +1,27 @@
+create table aluno
+(
+id int,
+nome varchar (255),
+telefone char (11),
+endereco varchar (255),
+primary key (id)
+);
+
+create table professor
+(
+id int,
+nome varchar (255),
+telefone char (11),
+endereco varchar (255),
+primary key (id)
+);
+
+create table funcionário
+(
+id int,
+nome varchar (255),
+telefone char (11),
+endereco varchar (255),
+primary key (id)
+);
+
